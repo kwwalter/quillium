c1ff4adc503ff8540810ecd19686ab7f2360d3b6
Kevin Walter <kevinwalter0731@gmail.com>
2016-01-08 16:26:35 -0500

    made one change to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :username, presence: true, uniqueness: true
-  validates :password_digest, length: { minimum: 8, allow_nil: true}
+  validates :password_digest, presence: true
+
+  validates :password, length: { minimum: 8, allow_nil: true }
 
   has_many :stories, dependent: :destroy
   has_many :edits, through: :stories, dependent: :destroy
